963157db48d9715433b4c53b7155aa5aa1132dd8
John_Anderson <John.anderson@lsgskychefs.com>
2026-04-08 13:05:51 -0500

    added verbose output

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
         raise RuntimeError("key not found")
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     # Now we can access `args.user_prompt`
 
@@ -24,13 +25,17 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     response = client.models.generate_content(model="gemini-3-flash-preview", contents=messages)
+    
     if response.usage_metadata is None:
         raise RuntimeError("usage metadata not found")
     
-    print (f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print (f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print (response.text)
-
+    if args.verbose:
+        print (f"User prompt: {args.user_prompt}")
+        print (f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print (f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print (response.text)
+    else:
+        print (response.text)
 
 if __name__ == "__main__":
     main()
